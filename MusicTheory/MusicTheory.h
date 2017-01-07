b0a6d5526ce393ac0a5bbc1f2bad5eaebe6e3971
cemolcay <ccemolcay@gmail.com>
2017-01-07 20:35:02 +0300

    Update framework hedaer

--- MusicTheory/MusicTheory.h
+++ MusicTheory/MusicTheory.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 prototapp. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MusicTheory.
 FOUNDATION_EXPORT double MusicTheoryVersionNumber;
