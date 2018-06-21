cb9707ae340a446ed8c12367e71ad6f678b44086
cemolcay <ccemolcay@gmail.com>
2018-06-21 20:37:54 +0300

    Fix pitch documentation

--- Source/Pitch.swift
+++ Source/Pitch.swift
@@ -10,92 +10,93 @@
 
 import Foundation
 
-/// Calculates the `Note` above `Interval`.
+/// Calculates the `Pitch` above `Interval`.
 ///
 /// - Parameters:
-///   - note: The note being added interval.
+///   - note: The pitch that is being added interval.
 ///   - interval: Interval above.
-/// - Returns: Returns `Note` above interval.
+/// - Returns: Returns `Pitch` above interval.
 public func +(pitch: Pitch, interval: Interval) -> Pitch {
   return Pitch(midiNote: pitch.rawValue + interval.rawValue)
 }
 
-/// Calculates the `Note` above halfsteps.
+/// Calculates the `Pitch` above halfsteps.
 ///
 /// - Parameters:
-///   - note: The note being added halfsteps.
+///   - note: The pitch that is being added halfsteps.
 ///   - halfstep: Halfsteps above.
-/// - Returns: Returns `Note` above halfsteps.
+/// - Returns: Returns `Pitch` above halfsteps.
 public func +(pitch: Pitch, halfstep: Int) -> Pitch {
   return Pitch(midiNote: pitch.rawValue + halfstep)
 }
 
-/// Calculates the `Note` below `Interval`.
+/// Calculates the `Pitch` below `Interval`.
 ///
 /// - Parameters:
-///   - note: The note being calculated.
+///   - note: The pitch that is being calculated.
 ///   - interval: Interval below.
-/// - Returns: Returns `Note` below interval.
+/// - Returns: Returns `Pitch` below interval.
 public func -(pitch: Pitch, interval: Interval) -> Pitch {
   return Pitch(midiNote: pitch.rawValue - interval.rawValue)
 }
 
-/// Calculates the `Note` below halfsteps.
+/// Calculates the `Pitch` below halfsteps.
 ///
 /// - Parameters:
-///   - note: The note being calculated.
+///   - note: The pitch that is being calculated.
 ///   - halfstep: Halfsteps below.
-/// - Returns: Returns `Note` below halfsteps.
+/// - Returns: Returns `Pitch` below halfsteps.
 public func -(pitch: Pitch, halfstep: Int) -> Pitch {
   return Pitch(midiNote: pitch.rawValue - halfstep)
 }
 
-/// Calculates the interval between two notes.
+/// Calculates the interval between two pitches.
 /// Doesn't matter left hand side and right hand side note places.
 ///
 /// - Parameters:
 ///   - lhs: Left hand side of the equation.
 ///   - rhs: Right hand side of the equation.
-/// - Returns: `Intreval` between two notes. You can get the halfsteps from interval as well.
+/// - Returns: `Intreval` between two pitches. You can get the halfsteps from interval as well.
 public func -(lhs: Pitch, rhs: Pitch) -> Interval {
   return Interval(integerLiteral: abs(rhs.rawValue - lhs.rawValue))
 }
 
-/// Compares the equality of two notes by their types and octaves.
+/// Compares the equality of two pitches by their keys and octaves.
 ///
 /// - Parameters:
-///   - left: Left handside `Note` to be compared.
-///   - right: Right handside `Note` to be compared.
-/// - Returns: Returns the bool value of comparisation of two notes.
+///   - left: Left handside `Pitch` to be compared.
+///   - right: Right handside `Pitch` to be compared.
+/// - Returns: Returns the bool value of comparisation of two pitches.
 public func ==(left: Pitch, right: Pitch) -> Bool {
   return left.key == right.key && left.octave == right.octave
 }
 
-/// Note object with `NoteType` and octave.
-/// Could be initilized with midiNote.
+/// Pitch object with a `Key` and an octave.
+/// Could be initilized with MIDI note number and preferred accident type.
 public struct Pitch: RawRepresentable, Codable, Equatable, ExpressibleByIntegerLiteral, CustomStringConvertible {
 
-  /// Key of the note like C, D, A, B.
+  /// Key of the pitch like C, D, A, B with accidents.
   public private(set) var key: Key
 
-  /// Octave of the note.
+  /// Octave of the pitch.
   /// In theory this must be zero or a positive integer.
   /// But `Note` does not limit octave and calculates every possible octave including the negative ones.
   public private(set) var octave: Int
 
-  /// Initilizes the `Note` from midi note.
+  /// Initilizes the `Pitch` with MIDI note number.
   ///
   /// - Parameter midiNote: Midi note in range of [0 - 127].
+  /// - Parameter isPreferredAccidentSharps: Make it true if preferred accidents is sharps. Defaults true.
   public init(midiNote: Int, isPreferredAccidentSharps: Bool = true) {
     octave = (midiNote / 12) - 1
     key = Key(midiNote: midiNote, isPreferredAccidentSharps: isPreferredAccidentSharps) ?? 0
   }
 
-  /// Initilizes the `Note` with `NoteType` and octave
+  /// Initilizes the `Pitch` with `Key` and octave
   ///
   /// - Parameters:
-  ///   - type: The type of the note in `NoteType` enum.
-  ///   - octave: Octave of the note.
+  ///   - key: Key of the pitch.
+  ///   - octave: Octave of the pitch.
   public init(key: Key, octave: Int) {
     self.key = key
     self.octave = octave
@@ -119,6 +120,9 @@ public struct Pitch: RawRepresentable, Codable, Equatable, ExpressibleByIntegerL
     return key.rawValue + ((octave + 1) * 12)
   }
 
+  /// Initilizes the pitch with an integer value that represents the MIDI note number of the pitch.
+  ///
+  /// - Parameter rawValue: MIDI note number of the pitch.
   public init?(rawValue: Pitch.RawValue) {
     self = Pitch(midiNote: rawValue)
   }
@@ -127,6 +131,9 @@ public struct Pitch: RawRepresentable, Codable, Equatable, ExpressibleByIntegerL
 
   public typealias IntegerLiteralType = Int
 
+  /// Initilizes the pitch with an integer value that represents the MIDI note number of the pitch.
+  ///
+  /// - Parameter value: MIDI note number of the pitch.
   public init(integerLiteral value: Pitch.IntegerLiteralType) {
     self = Pitch(midiNote: value)
   }
