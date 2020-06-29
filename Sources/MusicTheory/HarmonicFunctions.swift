d2fd6dfcac2f9fe1e3399eb8305ee9c19a5ec3b6
cemolcay <ccemolcay@gmail.com>
2020-06-29 18:37:14 +0300

    Add Harmonic Functions

---
+++ Sources/MusicTheory/HarmonicFunctions.swift
@@ -0,0 +1,55 @@
+//
+//  HarmonicFunctions.swift
+//  
+//
+//  Created by Cem Olcay on 29/06/2020.
+//  Copyright © 2017 cemolcay. All rights reserved.
+//
+
+import Foundation
+
+/// Represents harmonic functions in music theory.
+public enum HarmonicFunctions: Int, Codable, CaseIterable {
+  /// First interval/chord. I in roman numeral.
+  case tonic
+  /// Second interval/chord. II in roman numeral.
+  case supertonic
+  /// Third interval/chord. III in roman numeral.
+  case mediant
+  /// Fourth interval/chord. IV in roman numeral.
+  case subdominant
+  /// Fifth interval/chord. V in roman numeral.
+  case dominant
+  /// Sixth interval/chord. VI in roman numeral.
+  case submediant
+  /// Seventh interval/chord. VII in roman numeral.
+  case leading
+
+  /// Represents tonic prolongation functions.
+  static let tonicProlongationFunctions: [HarmonicFunctions] = [.mediant, .submediant]
+
+  /// Represents the pre dominant functions.
+  static let predominantFunctions: [HarmonicFunctions] = [.submediant, .supertonic]
+
+  /// Represents the dominant functions
+  static let dominantFunctions: [HarmonicFunctions] = [.dominant, .leading]
+
+  var direction: [HarmonicFunctions] {
+    switch self {
+    case .tonic:
+      return HarmonicFunctions.allCases
+    case .supertonic:
+      return HarmonicFunctions.dominantFunctions
+    case .mediant:
+      return HarmonicFunctions.predominantFunctions + [.submediant]
+    case .subdominant:
+      return [.supertonic] + HarmonicFunctions.dominantFunctions
+    case .dominant:
+      return [.tonic]
+    case .submediant:
+      return HarmonicFunctions.predominantFunctions
+    case .leading:
+      return [.tonic, .supertonic, .dominant]
+    }
+  }
+}
